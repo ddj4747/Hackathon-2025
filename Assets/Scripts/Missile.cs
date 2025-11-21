af9c3acffac9c0bfd2cdecbe2c969618bfc3537b
ddj4747 <139778357+ddj4747@users.noreply.github.com>
2025-11-22 00:39:36 +0100

    A

---
+++ Assets/Scripts/Missile.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    public float speed;
+    public float damage;
+
+    private Vector2 dir = new();
+    private float lifeTimeTimer = 0;
+
+    private void BlowUp()
+    {
+
+    }
+
+    void Start()
+    {
+        dir = transform.rotation * Vector2.up;
+    }
+
+    void Update()
+    {
+        lifeTimeTimer += Time.deltaTime;
+        transform.position += new Vector3(dir.x * speed * Time.deltaTime, dir.y * speed * Time.deltaTime);
+
+        if (lifeTimeTimer > 5)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "enemy")
+        {
+            Destroy(gameObject);
+            collision.gameObject.GetComponent<HealthComponent>().TakeDamage(damage);
+        }
+    }
+}
